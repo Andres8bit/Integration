b0422e7ffe7f1ece7d8c180955dbae2bbc431f50
andres8bit <andresC@cpp.edu>
2020-06-22 22:07:59 -0700

    added Romberg functions

--- integration.go
+++ integration.go
@@ -1,5 +1,7 @@
 package integration
 
+import "math"
+
 type NumVal float64
 type Func func(x NumVal) NumVal
 
@@ -43,6 +45,58 @@ func Trapizoid2(f func(x NumVal) NumVal, a, b NumVal, n int) NumVal {
 	sum = sum * h
 
 	return sum
+}
+
+//Romberg:
+// Uses Romberg's method to create an (n x n) array
+// In which numerical approximations of the integral is stored.
+// Unlike traditional Romberg's method this version transposes the matrix.
+// This is too avoid repeated cache misses, constructing matrix row wise rather than comlunm wise
+func Romberg(f []NumVal, a, b NumVal, r [][]NumVal) {
+	h := b - a
+	size := len(f) - 1
+	n := len(r)
+	fa := nested_Mutliplication(f[:], a, size)
+	fb := nested_Mutliplication(f[:], b, size)
+	r[0][0] = (h / 2) * (fa + fb)
+
+	for i := 1; i < n; i++ {
+		h = h / 2
+		sum := NumVal(0)
+		for k := 1; k < int(math.Pow(2, float64(i))); k = k + 2 {
+			fakh := nested_Mutliplication(f[:], NumVal(a+NumVal(k)*h), size)
+			sum = sum + fakh
+		}
+		r[i][0] = 0.5*r[i-1][0] + sum*h
+
+		for j := 1; j <= i; i++ {
+			r[i][j] = r[i][j-1] + (r[i][j-1]-r[i-1][j-1])/NumVal(math.Pow(4, float64(j))-1.0)
+		}
+
+	}
+}
+
+func Romberg2(f func(x NumVal) NumVal, a, b NumVal, r [][]NumVal) {
+	h := b - a
+	n := len(r)
+	fa := f(a)
+	fb := f(b)
+	r[0][0] = (h / 2) * (fa + fb)
+
+	for i := 1; i < n; i++ {
+		h = h / 2
+		sum := NumVal(0)
+		for k := 1; k < int(math.Pow(2, float64(i))); k = k + 2 {
+			fakh := f(a + NumVal(k)*h)
+			sum = sum + fakh
+		}
+		r[i][0] = 0.5*r[i-1][0] + sum*h
+
+		for j := 1; j <= i; i++ {
+			r[i][j] = r[i][j-1] + (r[i][j-1]-r[i-1][j-1])/NumVal(math.Pow(4, float64(j))-1.0)
+		}
+
+	}
 }
 
 // ======== Helper functions ===========
